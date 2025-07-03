48937ecee2fa43e1c9e8fc66fc764ec60ec03c13
marianarondone <marianarondone@gmail.com>
2025-07-03 15:28:45 -0400

    creacion del formulario
    definicion del estado inicial, creacion de interfaz evento

---
+++ src/app/Interfaces/IEvento.tsx
@@ -0,0 +1,7 @@
+export interface Evento{
+    nombreEvento: string,
+    participantes: number,
+    descripcion: string,
+    tipoEvento: string ,
+    fechaEvento: string
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,76 @@
+'use client'
+import { useState } from "react"
+import { Evento } from "./Interfaces/IEvento"
+
+let initialStateEvento:Evento = {
+  nombreEvento: "",
+  participantes: 0,
+  descripcion: "",
+  tipoEvento: "",
+  fechaEvento:""
+
+}
 
 
 export default function Home() {
-  return ;
+  const [evento, setEvento] = useState(initialStateEvento)
+
+
+  const handleEvento = (name: string, value: string | number) => {
+    console.log(name)
+    console.log(value)
+    setEvento(prev =>
+      ({...prev, [name]: value}
+    )
+  )}
+  return (
+    <>
+    <form>
+      <br/>
+      <label>Nombre del Evento</label>
+      <br/>
+      <input
+        name="nombreEvento"
+        type="text"
+        onChange={(e) => handleEvento(e.currentTarget.name, e.currentTarget.value)}/>
+      
+      <br/>
+      <label>Cantidad de Participantes</label>
+      <br/>
+      <input
+      name="participantes"
+      type="number"
+      onChange={(e) => handleEvento(e.currentTarget.name, e.currentTarget.value)}/>
+
+      <br/>
+      <label>Descripción</label>
+      <br/>
+      <textarea
+      name="descripcion"
+      onChange={(e) => handleEvento(e.currentTarget.name, e.currentTarget.value)}/>
+
+      <br/>
+      <label>Tipo de Evento</label>
+      <br/>
+      <select
+      name="tipoEvento"
+      onChange={(e) => handleEvento(e.currentTarget.name, e.currentTarget.value)}>
+
+      <option value="educativo">Educativo</option>
+      <option value = "cultural">Cultural</option>
+      <option value="deportivo">Deportivo</option>
+      <option value="otro">Otro</option>
+      </select>
+      <br/>
+      <label>Fecha de Evento</label>
+      <br/>
+      <input
+      name="fechaEvento"
+      type="date"
+      onChange={(e) => handleEvento(e.currentTarget.name, e.currentTarget.value)}/>
+      <span></span><br/>
+      <button>Guardar</button>
+    </form>
+    </>
+  )
 }
