fbaf99c59265c1f6b80e7a1c3d1c61d30664259a
marianarondone <marianarondone@gmail.com>
2025-07-07 16:22:41 -0400

    Adición de comentarios en el codigo

--- src/app/Componentes/MostrarEventos.tsx
+++ src/app/Componentes/MostrarEventos.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Evento } from '../Interfaces/IEvento'
 
-interface Props {
-  eventos: Evento[];
-  editar: (evento: Evento, index: number) => void;
-  eliminar: (index: number) => void;
+interface Props { //se definen los datos que este componente recibe desde el componente principal
+  eventos: Evento[]; //lista de eventos
+  editar: (evento: Evento, index: number) => void; //funcion de editar
+  eliminar: (index: number) => void; //funcion de eliminar
 }
 
-export const MostrarEventos = (props: Props) => {
+export const MostrarEventos = (props: Props) => { //recibe las propiedades desde el componente principal
 
-  const queEditar = (evento: Evento, index: number) => {
+  const queEditar = (evento: Evento, index: number) => { //llama a la funcion editar con el evento y su respectivo indice
     props.editar(evento, index)
   }
 
   return (
     <>
       <h1>LISTA DE EVENTOS</h1>
-      {props.eventos.length === 0 ? (
+      {props.eventos.length === 0 ? ( //verifica que haya eventos, si no hay muestra el mensaje de no hay eventos registrados
         <p>NO HAY EVENTOS REGISTRADOS</p>
       ) : (
         <table>
@@ -30,7 +30,7 @@ export const MostrarEventos = (props: Props) => {
             </tr>
           </thead>
           <tbody>
-            {props.eventos.map((e, index) => (
+            {props.eventos.map((e, index) => ( //usando el indice identifica cada fila que la funcion map extrae
               <tr key={index}>
                 <td>{e.nombreEvento}</td>
                 <td>{e.participantes}</td>
@@ -38,8 +38,8 @@ export const MostrarEventos = (props: Props) => {
                 <td>{e.tipoEvento}</td>
                 <td>{e.fechaEvento}</td>
                 <td>
-                  <button onClick={() => queEditar(e, index)}>Editar</button>
-                  <button onClick={() => props.eliminar(index)}>Eliminar</button>
+                  <button onClick={() => queEditar(e, index)}>Editar</button> {/*llama a queEditar que a la vez llama a la funcion editar*/}
+                  <button onClick={() => props.eliminar(index)}>Eliminar</button> {/*llama directamente a la funcion eliminar*/}
                 </td>
               </tr>
             ))}
